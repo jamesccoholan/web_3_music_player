2a9a0e84c3a613c112d0499ed8955dbc9a2439f3
James Coholan <jamescoholan@gmail.com>
2022-04-21 08:35:57 -0700

    Update fontend

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,5 +1,9 @@
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  const toWei = (num) => ethers.utils.parseEther(num.toString())
+  let royaltyFee = toWei(0.01); // 1 ether = 10^18 wei
+  let prices = [toWei(1), toWei(2), toWei(3), toWei(4), toWei(5), toWei(6), toWei(7), toWei(8)]
+  let deploymentFees = toWei(prices.length * 0.01)
+  const [deployer, artist] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
